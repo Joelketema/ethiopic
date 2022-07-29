adc849714e9a447c3f9f9eb586753a20e8664449
eyuel <eyuelket2019@gmail.com>
2022-07-29 08:28:23 -0900

    first commit

--- extension.js
+++ extension.js
@@ -11,11 +11,11 @@ function activate(context) {
 
 	let disposable = vscode.commands.registerCommand('ethiopic.date', function () {
 
-		const m = new Intl.DateTimeFormat("am", {calendar: "ethiopic", month: "long" }).format()
-		const d = new Intl.DateTimeFormat("am", {calendar: "ethiopic", day:"numeric" }).format()
-		const y = new Intl.DateTimeFormat("am", {calendar: "ethiopic", year:"numeric" }).format()
+		const month = new Intl.DateTimeFormat("am", {calendar: "ethiopic", month: "long" }).format()
+		const day = new Intl.DateTimeFormat("am", {calendar: "ethiopic", day:"numeric" }).format()
+		const year = new Intl.DateTimeFormat("am", {calendar: "ethiopic", year:"numeric" }).format()
 		
-		vscode.window.showInformationMessage("ዛሬ - " + m + " " + d +" " +y);
+		vscode.window.showInformationMessage("ዛሬ - " + month + " " + day +" " +year);
 	});
 
 	context.subscriptions.push(disposable);
